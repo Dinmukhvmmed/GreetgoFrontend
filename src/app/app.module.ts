9de5c50244a1f6ec9925a187ea35a47566326e4d
dinmukhammed_ospanov <dinmukhvmmed176@gmail.com>
2023-05-28 14:40:54 +0600

    Add HTML and TypeScript component for Backend controllers ("/"), import Angular Material button and input field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataComponent } from './data/data.component';
+import {MatButtonModule} from "@angular/material/button";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,11 @@ import { DataComponent } from './data/data.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import {Component, NgModule, OnInit} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {HttpClient} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
-  styleUrls: ['./data.component.css']
+  //styleUrls: ['./data.component.css']
 })
-export class DataComponent {
 
+export class DataComponent implements OnInit{
+  students: Student[] = [];
+
+  newStudent: Student = {
+    id: 0,
+    name: '',
+    surname: '',
+    age: 0,
+    averageGrade: 0
+  };
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.getAllStudents();
+  }
+
+  getAllStudents() {
+    this.http.get<Student[]>('/').subscribe(
+      response => {
+        this.students = response;
+      },
+      error => {
+        console.error('Failed to retrieve students:', error);
+      }
+    );
+  }
+
+  getStudent(id: number) {
+    this.http.get<Student>(`/${id}`).subscribe(
+      response => {
+        console.log('Student:', response);
+      },
+      error => {
+        console.error(`Failed to retrieve student with ID ${id}:`, error);
+      }
+    );
+  }
+
+
+  addStudent() {
+    this.http.post('/', this.newStudent).subscribe(
+      () => {
+        this.getAllStudents(); // Обновляем список студентов после добавления
+        this.resetForm(); // Сбрасываем значения формы
+      },
+      error => {
+        console.error('Failed to add student:', error);
+      }
+    );
+  }
+
+  resetForm() {
+    this.newStudent = {
+      id: 0,
+      name: '',
+      surname: '',
+      age: 0,
+      averageGrade: 0
+    };
+  }
+  deleteStudent(id: number) {
+    this.http.delete(`/${id}`).subscribe(
+      () => {
+        this.getAllStudents(); // Refresh the student list after deletion
+      },
+      error => {
+        console.error(`Failed to delete student with ID ${id}:`, error);
+      }
+    );
+  }
 }
+
+interface Student {
+  id: number;
+  name: string;
+  surname: string;
+  age: number;
+  averageGrade: number;
+}
+
