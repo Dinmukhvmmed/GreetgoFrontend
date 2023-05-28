637fd38cd9a750a4e05791db2c3c8005477751c0
dinmukhammed_ospanov <dinmukhvmmed176@gmail.com>
2023-05-28 12:24:57 +0600

    Init frontend project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DataComponent} from "./data/data.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DataComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
